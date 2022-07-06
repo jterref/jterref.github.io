ca24c08caf125737d78a799062ea5c9bd17933d8
jterref <90125386+jterref@users.noreply.github.com>
2022-07-06 01:01:06 -0300

    Change FDTG to Prompt Generator

--- script/header.js
+++ script/header.js
@@ -8,7 +8,7 @@ document.getElementById("header").innerHTML =
     + "    <ul>"
     + "        <li><a href='/'>HOME</a></li>"
     + "        <li><a href='gallery.html'>GALLERY</a></li>"
-    + "        <li><a href='fdtg.html'>FDTG</a></li>"
+    + "        <li><a href='fdtg.html'>PROMPT GENERATOR</a></li>"
     + "        <li><a href='contact.html'>CONTACT</a></li>"
     + "    </ul>"
     + "</nav>";
